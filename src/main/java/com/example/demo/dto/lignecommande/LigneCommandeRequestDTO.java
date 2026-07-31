5d400be6563f0a1e5e3edd83ca860a8b1fce1cf6
hibaHAJAJI <wiamhiba08@gmail.com>
2026-07-31 21:45:33 +0100

    add ligneCommande request and response DTOs

--- src/main/java/com/example/demo/dto/lignecommande/LigneCommandeRequestDTO.java
+++ src/main/java/com/example/demo/dto/lignecommande/LigneCommandeRequestDTO.java
@@ -1,4 +1,17 @@
 package com.example.demo.dto.lignecommande;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LigneCommandeRequestDTO {
+
+    private int quantite;
+
+    private Long commandeId;
+
+    private Long produitId;
 }
--- src/main/java/com/example/demo/dto/lignecommande/LigneCommandeResponseDTO.java
+++ src/main/java/com/example/demo/dto/lignecommande/LigneCommandeResponseDTO.java
@@ -1,4 +1,19 @@
 package com.example.demo.dto.lignecommande;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LigneCommandeResponseDTO {
+
+    private Long id;
+
+    private int quantite;
+
+    private Long commandeId;
+
+    private Long produitId;
 }
