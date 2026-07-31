7c2872c58cfdc63f2cae3934ddf426e9e4d40612
hibaHAJAJI <wiamhiba08@gmail.com>
2026-07-31 22:21:35 +0100

    add ligneCommande dto mappers

--- src/main/java/com/example/demo/mapper/LigneCommandeMapper.java
+++ src/main/java/com/example/demo/mapper/LigneCommandeMapper.java
@@ -1,8 +1,15 @@
 package com.example.demo.mapper;
 
 
+import com.example.demo.dto.lignecommande.LigneCommandeRequestDTO;
+import com.example.demo.dto.lignecommande.LigneCommandeResponseDTO;
+import com.example.demo.entity.LigneCommande;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel= "spring")
 public interface LigneCommandeMapper {
+
+    LigneCommande toEntity(LigneCommandeRequestDTO dto);
+
+    LigneCommandeResponseDTO toDto(LigneCommande ligneCommande);
 }
