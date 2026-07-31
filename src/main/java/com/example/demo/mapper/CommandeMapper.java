6ddabcb6176d340ac7a574065d82ad104ed8750f
hibaHAJAJI <wiamhiba08@gmail.com>
2026-07-31 22:19:10 +0100

    add commande dto mappers

--- src/main/java/com/example/demo/mapper/CommandeMapper.java
+++ src/main/java/com/example/demo/mapper/CommandeMapper.java
@@ -1,8 +1,15 @@
 package com.example.demo.mapper;
 
 
+import com.example.demo.dto.commande.CommandeRequestDTO;
+import com.example.demo.dto.commande.CommandeResponseDTO;
+import com.example.demo.entity.Commande;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel= "spring")
 public interface CommandeMapper {
+
+    Commande toEntity(CommandeRequestDTO dto);
+
+    CommandeResponseDTO toDto(Commande commande);
 }
