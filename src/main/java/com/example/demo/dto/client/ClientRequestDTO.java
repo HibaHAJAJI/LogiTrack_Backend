936194abf971b8d6f28f8cabfbae7139779bdbc5
hibaHAJAJI <wiamhiba08@gmail.com>
2026-07-31 21:30:18 +0100

    feat: add client request and response DTOs

--- src/main/java/com/example/demo/dto/client/ClientRequestDTO.java
+++ src/main/java/com/example/demo/dto/client/ClientRequestDTO.java
@@ -1,4 +1,21 @@
 package com.example.demo.dto.client;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ClientRequestDTO {
+
+    private String nom;
+
+    private String email;
+
+    private String telephone;
+
+    private String ville;
+
 }
--- src/main/java/com/example/demo/dto/client/ClientResponseDTO.java
+++ src/main/java/com/example/demo/dto/client/ClientResponseDTO.java
@@ -1,4 +1,25 @@
 package com.example.demo.dto.client;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ClientResponseDTO {
+
+        private Long id;
+
+        private String nom;
+
+        private String email;
+
+        private String telephone;
+
+        private String ville;
+
+
+
 }
