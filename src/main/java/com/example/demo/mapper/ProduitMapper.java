639fc0b11c4b0df7882bd9aa1dfc37a896a09bc2
hibaHAJAJI <wiamhiba08@gmail.com>
2026-07-31 22:23:25 +0100

    add produit dto mappers

--- src/main/java/com/example/demo/mapper/ProduitMapper.java
+++ src/main/java/com/example/demo/mapper/ProduitMapper.java
@@ -1,9 +1,15 @@
 package com.example.demo.mapper;
 
 
+import com.example.demo.dto.produit.ProduitRequestDTO;
+import com.example.demo.entity.Produit;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel= "spring")
 public interface ProduitMapper {
+
+    Produit toEntity(ProduitRequestDTO dto);
+
+    ProduitRequestDTO toDto(Produit produit);
 
 }
