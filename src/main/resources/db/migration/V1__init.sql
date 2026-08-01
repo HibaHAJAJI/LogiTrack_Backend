20271cbb3310da2581568a4aefd849bbc6cf548e
hibaHAJAJI <wiamhiba08@gmail.com>
2026-08-01 16:42:19 +0100

    add initial database migration

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,41 @@
+CREATE TABLE client (
+                        id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                        nom VARCHAR(50) NOT NULL,
+                        email VARCHAR(100) NOT NULL UNIQUE ,
+                        telephone VARCHAR(50),
+                        ville VARCHAR(50)
+);
+
+CREATE TABLE produit (
+                         id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                         nom VARCHAR(100) NOT NULL,
+                         categorie VARCHAR(255),
+                         prix DOUBLE NOT NULL,
+                         quantite_stock INT NOT NULL
+);
+
+CREATE TABLE commande (
+                          id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                          date_commande DATE,
+                          statut VARCHAR(50),
+                          client_id BIGINT NOT NULL,
+
+                          CONSTRAINT fk_commande_client
+                              FOREIGN KEY (client_id)
+                                  REFERENCES client(id)
+);
+
+CREATE TABLE ligne_commande (
+                                id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                quantite INT NOT NULL,
+                                commande_id BIGINT NOT NULL,
+                                produit_id BIGINT NOT NULL,
+
+                                CONSTRAINT fk_ligne_commande
+                                    FOREIGN KEY (commande_id)
+                                        REFERENCES commande(id),
+
+                                CONSTRAINT fk_ligne_produit
+                                    FOREIGN KEY (produit_id)
+                                        REFERENCES produit(id)
+);
