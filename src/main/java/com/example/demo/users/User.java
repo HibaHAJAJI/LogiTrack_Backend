65d899d3de6836e5fed35180b63a3f03f27adb76
hibaHAJAJI <wiamhiba08@gmail.com>
2026-08-02 01:29:03 +0100

    add user roles

--- src/main/java/com/example/demo/users/User.java
+++ src/main/java/com/example/demo/users/User.java
@@ -1,4 +1,34 @@
 package com.example.demo.users;
 
+
+import com.example.demo.Enum.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class User {
+
+     @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nom;
+
+    private String prenom;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }
