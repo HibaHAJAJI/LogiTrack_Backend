a0ffcb36d3da51d6c33ccd85561444d2cb169245
hibaHAJAJI <wiamhiba08@gmail.com>
2026-08-03 15:00:07 +0100

    add authentication DTO

--- src/main/java/com/example/demo/auth/dto/AuthResponse.java
+++ src/main/java/com/example/demo/auth/dto/AuthResponse.java
@@ -1,4 +1,20 @@
 package com.example.demo.auth.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthResponse {
+
+    private Long id;
+
+    private String email;
+
+    private String role;
+
+    private String token;
 }
