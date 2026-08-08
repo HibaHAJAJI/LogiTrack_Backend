c053953dda5d305094c0884ac822a93d7e6ce190
hibaHAJAJI <wiamhiba08@gmail.com>
2026-08-08 19:01:51 +0100

    add clientId mapping

--- src/main/java/com/example/demo/mapper/CommandeMapper.java
+++ src/main/java/com/example/demo/mapper/CommandeMapper.java
@@ -5,6 +5,7 @@ import com.example.demo.dto.commande.CommandeRequestDTO;
 import com.example.demo.dto.commande.CommandeResponseDTO;
 import com.example.demo.entity.Commande;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ public interface CommandeMapper {
 
     Commande toEntity(CommandeRequestDTO dto);
 
+    @Mapping(source = "client.id", target = "clientId")
     CommandeResponseDTO toDto(Commande commande);
 
     List<CommandeResponseDTO> toDtoList(List<Commande> commandes);
