b04aff79dcf5df54835f6881d8cb791c08fcde43
hibaHAJAJI <wiamhiba08@gmail.com>
2026-08-31 16:26:28 +0100

    feat: initialize logitrack-notification-service and feign client integration

--- src/main/java/com/example/demo/dto/user/UserRegisterRequest.java
+++ src/main/java/com/example/demo/dto/user/UserRegisterRequest.java
@@ -25,7 +25,7 @@ public class UserRegisterRequest {
     private String email;
 
     @NotBlank(message = "Le mot de passe est obligatoire")
-    @Size(min = 8, message = "Le mot de passe doit contenir au moins 8 caractères")
+    @Size(min = 6, message = "Le mot de passe doit contenir au moins 8 caractères")
     private String password;
 
     private Role role;
