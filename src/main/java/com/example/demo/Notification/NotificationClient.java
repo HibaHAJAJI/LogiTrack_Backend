301a8ef610547b19db67a3b936a7ad6645a87da3
hibaHAJAJI <wiamhiba08@gmail.com>
2026-09-02 15:28:55 +0100

    feat: add notification feign client

--- src/main/java/com/example/demo/Notification/NotificationClient.java
+++ src/main/java/com/example/demo/Notification/NotificationClient.java
@@ -1,4 +1,14 @@
 package com.example.demo.Notification;
 
-public class NotificationClient {
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@FeignClient(name = "notification-service", url = "${notification.service.url}")
+public interface NotificationClient {
+
+    @PostMapping("/api/notifications")
+    NotificationDTO sendNotification(@RequestBody NotificationDTO notification);
 }
